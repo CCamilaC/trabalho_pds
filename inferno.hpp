2dd9e8de86e2a10c1fe3456e5d9f39b82ca55b11
Luiza <luizalei8@gmail.com>
2022-10-19 10:39:31 -0300

    inferno

---
+++ inferno.hpp
@@ -0,0 +1,15 @@
+#ifndef inferno
+#define inferno
+
+class inferno: public historia
+{
+    public:
+    void inicializar_historia();
+    int Preguica();
+    int Gula();
+    int Soberba(); 
+    int Luxuria(); 
+    int Avareza(); 
+    int Inveja(); 
+    int Ira();
+}
