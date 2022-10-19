661aea07c62ed8b506dceb18bc96698847261c50
Luiza <luizalei8@gmail.com>
2022-10-19 10:22:02 -0300

    classe historia

---
+++ historia.hpp
@@ -0,0 +1,15 @@
+//Classe
+#ifndef historia
+#define historia 
+#include<iostream>
+#include<string>
+
+class historia
+{
+    protected: 
+
+    public:
+    void inicializar_historia();
+    void finalizar_historia();
+}
+#endif
