437fa283eb41130d50791f0889f9237836123565
Camila <camilachagascarvalho888@gmail.com>
2022-11-23 07:52:02 -0300

    utilizando doxygen na classe inferno

--- hpp/inferno.hpp
+++ hpp/inferno.hpp
@@ -1,7 +1,9 @@
 /**
  * @file inferno.hpp
- * @author your name (you@domain.com)
- * @brief 
+ * @author Camila Chagas (camilachagascarvalho888@gmail.com) Luiza lei (luizalei8@gmail.com)
+ * @brief a classe inferno é responsável por toda narrativa quando o jogador estiver ambientado no inferno
+ * @details na classe inferno haverá todos os prints da narrativa, juntamente com os métodos de escolha para
+ *  ao longo da história, além de chamar funções da classe batalha para participar da história
  * @version 0.1
  * @date 2022-11-18
  * 
@@ -16,9 +18,16 @@
 #include <string>
 #include "historia.hpp"
 #include "batalha.hpp"
-
+/** 
+ * @brief printa na tela toda a história do inferno, implementa batalhas e faz as escolhas do jogo
+ * @details essa classe possui um vetor para armazenar todas as escolhas ao longo da implementação das funções da classe
+*/
 class Inferno: public Historia
 {
+    std::vector<int> _choice; //variável que armazena as escolhas do usuário
+    int _contador; //auxiliar para determinar quantas escolhas boas foram
+                  //feitas pelo usuário
+    std::string _escolha="ESCOLHA 1 OU 2:\n------------------------\\------------------------";
     public:
     Inferno();
     int Escolha();
@@ -32,10 +41,6 @@ class Inferno: public Historia
     void Batalha_final();
     Batalha batalha;
 
-    private:
-    std::vector<int> _choice; //variável que armazena as escolhas do usuário
-    int _contador; //auxiliar para determinar quantas escolhas boas foram
-                  //feitas pelo usuário
-    std::string _escolha="ESCOLHA 1 OU 2:\n------------------------\\------------------------";
+
 };
 #endif 
