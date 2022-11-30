658385c7935220d2931814a88778b671bd043581
luisotavio123 <113053832+luisotavio123@users.noreply.github.com>
2022-11-30 19:46:58 -0300

    Create inventario.cpp

---
+++ hpp/inventario.cpp
@@ -0,0 +1,19 @@
+#include "inventario.hpp"
+
+        string item:: getNome(){
+            return _nome;
+        }
+
+        int item:: getTipo(){
+            return _tipo;
+        }
+
+        int item:: getEfeito(){
+            return _efeito;
+        }
+
+        item::item(string x, int y, int z){
+            _nome = x;
+            _tipo = y;
+            _efeito = z;
+        }
