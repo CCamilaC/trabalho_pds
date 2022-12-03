dd56e6a94eb0fc5bb69f0acdae94ca8847f9ef58
Natan_HCR <nhcr7@hotmail.com>
2022-12-03 18:29:38 -0300

    Testando História

--- hpp/historia.hpp
+++ hpp/historia.hpp
@@ -1,15 +1,20 @@
 //Classe
 #ifndef Historia_H
 #define Historia_H
+#include "jogador.hpp"
 
 #include<iostream>
 #include<string>
 
 class Historia
 {
+    private:
+    Jogador _jogador;
+    
     public:
+    Historia(Jogador jogador);
     void inicializar_historia();
-    virtual void finalizar_historia();
+    void finalizar_historia();
 };
 
 #endif
