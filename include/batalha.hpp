100d847fc6135e1e4d601b176637eadfaba1d42b
Pedroh404 <pedrohenrique.s.lima@hotmail.com>
2022-12-05 18:33:45 -0300

    Loop de batalha configurado

--- include/batalha.hpp
+++ include/batalha.hpp
@@ -9,12 +9,13 @@
 class Batalha{
 
 public:
-    bool atacar(Jogador *user, Jogador *inim);
+    bool atacar(Jogador *user, Jogador *inim, bool atq_def);
     bool defender(Jogador *user, Jogador *inim);
     bool inventario(Jogador *user, Jogador *inim);
-    void envenenado(Jogador *user, Jogador *inim );
 
-    void batalhar(Jogador *user, Jogador *inim);
+
+    int batalhar(Jogador *user, Jogador *inim);
+
 };
 
 
--- src/batalha.cpp
+++ src/batalha.cpp
@@ -1,63 +1,125 @@
 #include "batalha.hpp"
 
 using namespace std;
+int cont = 0;
 
-void Batalha::batalhar(Jogador *user, Jogador *inim){
+int Batalha::batalhar(Jogador *user, Jogador *inim){
     int escolha;
     int escolha_inim;
+    escolha_inim = (rand() % 2);
+
     cout << "\n" << "escolha entre:" <<"\n\n"<<"[1] = Atacar"<<"\n\n"<<"[2] = Defender "<< "\n\n" << "[3] = Usar item" <<"\n\n";
     cin >> escolha;
-    if (escolha == 1){
-        atacar(user, inim);
+    inim->envenenado(cont);
+    cont++;
+    if(inim->get_vida() == 0){
+        cout << "Voçê derrotou o inimigo" << endl;
+        return 2;
+    }
+    
+    if (escolha == 1 && escolha_inim == 0){
+        
+        cout << "O inimigo atacou" << endl;
+        cout << "------------------------------------------------------" << endl;
+        if(atacar(user, inim, 0)){
+            cout << "Voçê derrotou o inimigo" << endl;
+            return 2;
+        }
+        if(atacar(inim, user, 0)){
+            cout << "Voçê foi derrotado" << endl;
+            return 0;
+        };
         
 
         user->printv();
         inim->printiv();
     }
+    else if (escolha == 1 && escolha_inim == 1){
+        
+        cout << "O inimigo defendeu" << endl;
+        cout << "------------------------------------------------------" << endl;
+        if(atacar(user, inim, 1)){
+            cout << "Voçê derrotou o inimigo" << endl;
+            return 2;
+        }
+        defender(inim, user);
+        
 
-    else if(escolha == 2){
+        user->printv();
+        inim->printiv();
+    }    
+
+    else if(escolha == 2 && escolha_inim == 0){
+        
+        cout << "O inimigo atacou" << endl;
+        cout << "------------------------------------------------------" << endl;
         defender(user, inim);
+        if(atacar(inim, user, 1)){
+            cout << "Voçê foi derrotado" << endl;
+            return 0;
+        }
         
         
         user->printv();
         inim->printiv();
 
     }
+    else if(escolha == 2 && escolha_inim == 1){
+        
+        cout << "O inimigo defendeu" << endl;
+        cout << "------------------------------------------------------" << endl;
+        defender(user, inim);
+        defender(inim, user);
+        
+        
+        user->printv();
+        inim->printiv();
 
-    else if(escolha == 3){
-
+    }
+    else if(escolha == 3 && escolha_inim == 0){
+        
         while(inventario(user, inim) == 0){
             batalhar(user, inim);
+        }
+        
+        cout << "O inimigo atacou" << endl;
+        cout << "------------------------------------------------------" << endl;
+        if(atacar(inim, user, 0)){
+            cout << "Voçê foi derrotado" << endl;
+            return 0;
         }
         user->printv();
         inim->printiv();
     
     
     }
-
-    escolha_inim = (rand() % 2);
-    if(escolha_inim == 0){
-        cout << "O inimigo atacou" << endl;
-        cout << "------------------------------------------------------" << endl;
-        atacar(inim, user);
-
-    }
-    else if(escolha_inim == 1){
+    else if(escolha == 3 && escolha_inim == 1){
+        
+        while(inventario(user, inim) == 0){
+            batalhar(user, inim);
+        }
+        if(user->get_vida() == 0){
+            cout << "voçê foi derrotado" << endl;
+            return 0;
+        }
+        
         cout << "O inimigo defendeu" << endl;
         cout << "------------------------------------------------------" << endl;
         defender(inim, user);
-
+        user->printv();
+        inim->printiv();
+    
     }
+    return 1;
+    
 }
 
 
 
 
-        bool Batalha::atacar(Jogador *user, Jogador *inim){
-            int y = user->get_atq() - inim->get_def();
-            y = inim->get_vida() - y;
-            inim->set_vida(y);
 
+
+        bool Batalha::atacar(Jogador *user, Jogador *inim, bool atq_def){
             try{
             int x =  user->get_estamina() - 1;
             user->set_estamina(x);
@@ -67,34 +129,42 @@ void Batalha::batalhar(Jogador *user, Jogador *inim){
                 user->set_estamina(x);
             throw "sua estamina esta em zero, voce nao pode atacar. ";
             }
+            int y = user->get_atq();
+            if(atq_def){
+                y = y - inim->get_def();
+            }
+            y = inim->get_vida() - y;
+            inim->set_vida(y);
+
+            
 
             }catch(const char *e){
-                cout << e;
+                cout << e << endl;
+                batalhar(user, inim);
 
             }
             if (inim->get_vida() <= 0 || user->get_vida() <= 0){
-                cout << "a batalha acabou" << endl;
+
                 return true;
 
             }
-            cout<<"vida do " << inim->get_nome()<<":"<< inim->get_vida() <<"\n";
-            cout<<"estamina do "<< inim->get_nome()<<":" << inim->get_estamina() <<"\n" << "====================" << "\n";
-            user->print();
+            
             return false;
         }
 
         bool Batalha::defender(Jogador *user, Jogador *inim){
-            int y = inim->get_atq() - user->get_def();
-            int x = user->get_vida() - y;
-            user->set_vida(x);
+
             int z = user->get_estamina() + 1;
+            if(z > user->get_max_estamina()){
+                z = z - 1;
+            }
             user->set_estamina(z);
             if (inim->get_vida() <= 0 || user->get_vida() <= 0){
-                cout << "a batalha acabou";
+
                 return true;
 
             }
-            user->print();
+
             return false;
 
 
@@ -117,20 +187,3 @@ void Batalha::batalhar(Jogador *user, Jogador *inim){
 
         }
 
-
-        void Batalha::envenenado(Jogador *user, Jogador *inim){
-            int x = 0;
-            int aux = 5;
-            int cont;
-
-            if(cont < 5){
-                aux = aux - cont;
-                cout <<  "o inimigo esta envenenado por:"<< aux <<" rodadas"<< "\n";
-                x = inim->get_vida() - 1;
-                inim->set_vida(x);
-                ++cont;
-            }else{
-                cout << "o inimigo nao esta mais envenenado" << "\n";
-                bool y = false;
-                user->set_veneno(y);}
-        }
